58542134d32c330583610019eaa419ba5e791d59
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-10-15 06:51:28 -0700

    Add files via upload

---
+++ app/About/page.tsx
@@ -0,0 +1,17 @@
+
+
+
+// import HomeMain from "@/pages/homes/home/HomeMain";
+import AboutStartupMain from "@/pages/about/about-startup/AboutStartupMain";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us - Cheeltech Digital Solutions",
+};
+
+export default function About() {
+  return (
+    // <HomeMain />
+    <AboutStartupMain />
+  );
+}
---
+++ app/Home-page/page.tsx
@@ -0,0 +1,17 @@
+
+
+
+// import HomeMain from "@/pages/homes/home/HomeMain";
+import ITSolutionMain from "@/pages/homes/it-solution/ITSolutionMain";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home - Cheeltech Digital Agency",
+};
+
+export default function Home() {
+  return (
+    // <HomeMain />
+    <ITSolutionMain />
+  );
+}
